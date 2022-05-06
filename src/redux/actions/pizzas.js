b77e9768a4ff3fe77a378e4524238eba916f7d75
oliwkevich <olishkevich123@gmail.com>
2022-05-06 19:30:41 +0200

    fixes

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -1,7 +1,7 @@
 export const fetchPizzas = (sortBy, category) => (dispatch) => {
   dispatch(setLoaded(false));
   fetch(
-    `http://localhost:3001/pizzas?${
+    `/pizzas?${
       category !== null ? `category=${category}` : ''
     }&_sort=${sortBy.type}&_order=${sortBy.order}`
   )
